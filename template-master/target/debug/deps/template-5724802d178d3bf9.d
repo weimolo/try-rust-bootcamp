5a2bef94a97fa5e424f631b29d8807b09cf8134a
weimolo <47289227+weimolo@users.noreply.github.com>
2025-07-18 21:32:19 +0800

    Delete template-master directory

--- template-master/target/debug/deps/template-5724802d178d3bf9.d
+++
@@ -1,5 +0,0 @@
-C:\Users\weimo\Desktop\template-master\target\debug\deps\template-5724802d178d3bf9.d: src\main.rs
-
-C:\Users\weimo\Desktop\template-master\target\debug\deps\libtemplate-5724802d178d3bf9.rmeta: src\main.rs
-
-src\main.rs:
--- template-master/target/debug/deps/template-861b0bdc70e19e82.d
+++
@@ -1,5 +0,0 @@
-C:\Users\weimo\Desktop\template-master\target\debug\deps\template-861b0bdc70e19e82.d: src\main.rs
-
-C:\Users\weimo\Desktop\template-master\target\debug\deps\libtemplate-861b0bdc70e19e82.rmeta: src\main.rs
-
-src\main.rs:
