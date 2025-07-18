79d47d81aa6bed98af95f5fea4136e91ca3ac585
weimolo <47289227+weimolo@users.noreply.github.com>
2025-07-18 20:49:59 +0800

    Add files via upload

---
+++ template-master/target/debug/deps/template-861b0bdc70e19e82.d
@@ -0,0 +1,5 @@
+C:\Users\weimo\Desktop\template-master\target\debug\deps\template-861b0bdc70e19e82.d: src\main.rs
+
+C:\Users\weimo\Desktop\template-master\target\debug\deps\libtemplate-861b0bdc70e19e82.rmeta: src\main.rs
+
+src\main.rs:
